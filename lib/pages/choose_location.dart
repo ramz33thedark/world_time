bea6814b5171b7cf9954eb0a1856a3b442ff591f
ramz33 <aungphyopaing1500@gmail.com>
2022-08-09 21:13:39 +0630

    Add API

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton.icon(onPressed: (){
+        setState(() {
+          counter += 1;
+        });
+      }, 
+      icon: Icon(Icons.add), 
+      label: Text('Counter is $counter'))
+    );
+  }
+}
